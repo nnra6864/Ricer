55d66cf73e649f2a7f408a62337948c3fb9a073d
nnra6864 <enenragd@gmail.com>
2024-07-05 13:26:14 +0200

    Finished a piece of config

--- Config.py
+++ Config.py
@@ -20,11 +20,11 @@ class Config:
 
     @staticmethod
     def create_dir(dir):
-        print(f"{Fore.YELLOW}{dir} doesn't exist")
-        print(f"Creating {Fore.BLUE+dir}")
+        print(f"{Fore.BLUE}{dir} {Fore.YELLOW}doesn't exist")
+        print(f"Creating {Fore.BLUE}{dir}")
         try:
             os.makedirs(dir)
-            print(f"{Fore.GREEN}{dir} created successfully")
+            print(f"{Fore.BLUE}{dir} {Fore.GREEN}created successfully")
         except OSError as err:
             raise OSError(f"{Fore.RED}{err}")
 
@@ -34,9 +34,9 @@ class Config:
         if response.status_code == 200:
             with open(save_path, 'wb') as f:
                 f.write(response.content)
-            print(f"{Fore.GREEN}Downloaded {url} to {save_path}")
+            print(f"{Fore.GREEN}Downloaded {Fore.BLUE}{url}{Fore.GREEN} to {Fore.BLUE}{save_path}")
         else:
-            raise Exception(f"{Fore.RED}Failed to download {url}: {response}")
+            raise Exception(f"{Fore.RED}Failed to download {Fore.BLUE}{url}{Fore.RED}: {response}")
 
     @classmethod
     def load_config(cls):
@@ -64,27 +64,27 @@ class Config:
                 try:
                     Config.download_file(raw_template_link, template)
                 except Exception as ex:
-                    raise Exception(f"{ex}\n{Fore.RESET}Try manually downloading the file and placing it in the same dir as ColorRicer\nTemplate: {Fore.BLUE}{template_link}{Fore.RESET}\nColorRicer: {Fore.BLUE}") from None
+                    raise Exception(f"{ex}\n{Fore.RESET}Try manually downloading the file and placing it in the same dir as ColorRicer\nTemplate: {Fore.BLUE}{template_link}{Fore.RESET}\nColorRicer: {Fore.BLUE}{cfg_path}") from None
 
-            print(f"Moving {Fore.BLUE}{template}{Fore.RESET} to {Fore.BLUE}{cfg_path}")
+            print(f"Copying {Fore.BLUE}{template}{Fore.RESET} to {Fore.BLUE}{cfg_path}")
             shutil.copy(template, cfg_path)
-            print(Fore.GREEN + "Copied ColorRicer.toml to ~/.config/ColorRicer.toml")
+            print(f"{Fore.GREEN}Copied {Fore.BLUE}{template}{Fore.GREEN} to {Fore.BLUE}{cfg_path}")
         
         else:
-            print(Fore.GREEN + "Found config at ~/.config/ColorRicer.toml")
-
-        print("Loading config\n")
+            print(f"{Fore.GREEN}Found config at {Fore.BLUE}{cfg_path}")
+        
+        print("\nLoading config")
 
         with open(cfg_path, 'r') as cfg:
             config = toml.load(cfg)
 
         if "default_format" in config:
             cls.default_format = config["default_format"]
-            print(f"Loaded default format: {Fore.BLUE + cls.default_format}")
+            print(f"{Fore.GREEN}Loaded{Fore.RESET} default format: {Fore.BLUE + cls.default_format}")
     
         if "color_log_format" in config:
             cls.color_log_format = config["color_log_format"]
-            print(f"Loaded color_log_format: {Fore.BLUE + cls.color_log_format}")
+            print(f"{Fore.GREEN}Loaded{Fore.RESET} color_log_format: {Fore.BLUE + cls.color_log_format}")
     
         if "Colors" in config:
             print("\nLoading Colors")
@@ -97,7 +97,7 @@ class Config:
             cfg_files = config["Files"]
             for name, file in cfg_files.items():
                 if isinstance(file, str):
-                    cls.files.append(File(name, file, Config.format))
+                    cls.files.append(File(name, file, Config.default_format))
     
                 elif isinstance(file, list) and len(file) in [1, 2]:
                     form = cls.default_format if len(file) == 1 else file[1]
